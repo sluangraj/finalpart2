e90beba45f5a4b64e5a1f8e0e0c6959136497588
Stephanie Luangraj <sluangra@iu.edu>
2018-12-15 15:19:29 -0500

    finalpart2
    finalpart2

--- views/members/profile.php
+++ views/members/profile.php
@@ -6,7 +6,7 @@ include('views/elements/header.php');
 <div class="container">
 	<div class="page-header">
     <h1>Edit Profile</h1>
-   
+
     <?php if($message){?>
     <div class="alert alert-success">
     <button type="button" class="close" data-dismiss="alert">×</button>
@@ -17,6 +17,8 @@ include('views/elements/header.php');
     <form id="profile_edit_form" action="<?php echo BASE_URL;?>members/update<?php echo $task?>" method="post">
         <?php include('views/elements/edit_profile.php'); ?>
     </form>
+
+
     <?php echo '<p><a href="'.BASE_URL.'">Return home page</a></p>';?>
 
 </div>
